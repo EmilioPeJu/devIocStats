421181d98942cfd3bb0ea694ebae705eb991396d
tmc43 <tmc43@e099a375-04f9-0310-9d5f-a741eaff62e1>
2010-01-12 10:17:29 +0000

    devIocStats: updated and simplified builder definitions
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/devIocStats@40083 e099a375-04f9-0310-9d5f-a741eaff62e1

--- etc/builder.py
+++ etc/builder.py
@@ -1,17 +1,8 @@
-from iocbuilder import Substitution, Device
+from iocbuilder import AutoSubstitution, Device
 from iocbuilder.arginfo import *
 
-class ioc(Substitution, Device):
-    '''Creates vx statistics for the ioc.  To view the ioc statistics
-    run vxStatsApp/opi/edl/iocStatus.edl with the same device=device
-    substitution.'''
-    # __init__ arguments
-    ArgInfo = makeArgInfo(
-        IOCNAME = Simple('IOC name', str),
-        name    = Simple('Object name', str))
-    
+class ioc(AutoSubstitution, Device):
     # Substitution attributes
-    Arguments = ArgInfo.Names()
     TemplateFile = 'ioc.db'
     
     # Device attributes    
