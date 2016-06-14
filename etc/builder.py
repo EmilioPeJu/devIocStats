967167cc6b7100d1581e9055b1a57da0137e501c
qvr31998 <qvr31998@e099a375-04f9-0310-9d5f-a741eaff62e1>
2016-06-14 14:48:40 +0000

    Fixed the builder support to find the macros included from iocGui.db in ioc.template
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/devIocStats@148634 e099a375-04f9-0310-9d5f-a741eaff62e1

--- etc/builder.py
+++ etc/builder.py
@@ -1,11 +1,36 @@
 from iocbuilder import AutoSubstitution, Device, ModuleBase, Call_TargetOS
 from iocbuilder.arginfo import *
 
+# This decorator provides the ability to create a template class
+# that overrides another template.  Use this when a *.template file
+# has an include statement.  This functionality will be moved into
+# iocbuilder in the future
+# Shamelessly stolen from ADCore 2-4dls2 - EW 14/06/16
+def includesTemplates(*templates):
+    def decorator(cls):
+        arginfo = templates[0].ArgInfo
+        arguments = list(templates[0].Arguments)
+        defaults = templates[0].Defaults.copy()
+        for template in templates[1:]:
+            arginfo += template.ArgInfo
+            arguments += [x for x in template.Arguments if x not in arguments]
+            defaults.update(template.Defaults)
+        cls.ArgInfo = arginfo + cls.ArgInfo
+        cls.Arguments = list(cls.Arguments) + [x for x in arguments if x not in cls.Arguments]
+        defaults.update(cls.Defaults)
+        cls.Defaults = defaults
+        return cls
+    return decorator    
+
 class _devIocStats(Device):
     LibFileList = ['devIocStats']
     DbdFileList = ['devIocStats']
     AutoInstantiate = True
 
+class iocGui(AutoSubstitution):
+    TemplateFile = 'iocGui.db'
+
+@includesTemplates(iocGui)
 class ioc(AutoSubstitution):
     Dependencies = (_devIocStats,)
     TemplateFile = 'ioc.template'
